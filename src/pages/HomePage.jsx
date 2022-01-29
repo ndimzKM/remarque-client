230910f0d32b5521873dcba57981fa2dbe4ab184
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-01-29 21:38:13 +0000

    #12

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import {} from 'react-icons';
 
 const HomePage = () => {
   return (
     <div className="flex items-center flex-col">
-      <h1>Hello from HomePage</h1>
+      <h1>The simplest way to keep notes</h1>
+      <p>All your notes, synced on all your devices. Get Remarque now for iOS, Android, Mac, Windows, Linux, or in your browser.</p>
+      
       <Link to="/app">Go to App</Link>
     </div>
   );
