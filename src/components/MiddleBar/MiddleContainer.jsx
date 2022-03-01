b404bc1870e8e66b755899b9cf8ba436e642c80b
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-03-01 17:51:34 +0000

    search notes

--- src/components/MiddleBar/MiddleContainer.jsx
+++ src/components/MiddleBar/MiddleContainer.jsx
@@ -1,19 +1,31 @@
-import { AiOutlineSearch, AiOutlinePlus, AiOutlineBell } from "react-icons/ai";
-import {useDispatch, useSelector} from 'react-redux'
+import { useState } from "react";
+import { AiOutlineSearch, AiOutlinePlus } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
 import Notes from "./Notes";
-import ModalView from './Modal'
-import {toggleModal} from '../../redux/actions'
+import ModalView from "./Modal";
+import { toggleModal, updateTempNotes } from "../../redux/actions";
 const MiddleContainer = () => {
-  const dispatch = useDispatch()
-  const {modal} = useSelector((state) => state.reducer)
+  const dispatch = useDispatch();
+  const { tempNotes, notes } = useSelector((state) => state.reducer);
+  const [searchField, setSearchField] = useState("");
   const openModal = () => {
     dispatch(toggleModal(true));
-  }
+  };
+
+  const filterNotes = (value) => {
+    setSearchField(value);
+    let filteredNotes = notes.filter((note) =>
+      note.title.toLowerCase().includes(value.toLowerCase())
+    );
+    //console.log("Changes is happening");
+    if (value == "") dispatch(updateTempNotes(notes));
+    else dispatch(updateTempNotes(filteredNotes));
+  };
 
   return (
     <section className="middle-container py-3">
       <div className="top-search flex items-center px-2 justify-between">
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <div className="flex items-center">
             <div className="icon">
               <AiOutlineSearch size={20} color="#999" />
@@ -21,8 +33,9 @@ const MiddleContainer = () => {
             <input
               type="text"
               className="bg-transparent pl-1"
-              value=""
+              value={searchField}
               placeholder="Search"
+              onChange={(e) => filterNotes(e.target.value)}
             />
           </div>
         </form>
